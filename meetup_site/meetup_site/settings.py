6c23d6568ef10db6a4578cca098e7841c3e9cb6d
Coriolanus <savvarabota@gmail.com>
2023-03-15 14:21:56 +0300

    Добавил модель Company, создал базу данных

--- meetup_site/meetup_site/settings.py
+++ meetup_site/meetup_site/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'meetups'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- meetup_site/meetup_site/urls.py
+++ meetup_site/meetup_site/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-
+from meetup_site import settings
 from meetups.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
