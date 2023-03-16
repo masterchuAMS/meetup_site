32a4b4a33eb87bd005c402ee3a3656bc59a84965
Coriolanus <savvarabota@gmail.com>
2023-03-16 14:18:37 +0300

    Подключил css

--- meetup_site/meetups/views.py
+++ meetup_site/meetups/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
+menu = ["О сайте","Обратная свзязь","Войти"]
 def index(request):
-    return render(request, 'mysite/index.html')
+    posts = Company.objects.all()
+    return render(request, 'mysite/index.html',{'posts':posts,'menu':menu, 'title':'Главная страница'})
