dadf5fb13d407445ca6b039caf67db64a943a344
Coriolanus <savvarabota@gmail.com>
2023-03-24 10:07:46 +0300

    Добавил меню в виде базы данных и админ панель

--- meetup_site/meetups/admin.py
+++ meetup_site/meetups/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from .models import *
+
+
 # Register your models here.
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id','title','time_create','photo')
-    list_display_links = ('id','title')
-    search_fields = ('title','content')
+    list_display = ('id', 'title', 'time_create', 'photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
     list_filter = ('time_create',)
 
-
+a='sdasdasda'
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','name')
+    list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
 
-admin.site.register(Company,CompanyAdmin)
-admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Category, CategoryAdmin)
