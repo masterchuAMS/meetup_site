a202b5c4b520b4a9974fae863a988385128a9067
Coriolanus <savvarabota@gmail.com>
2023-03-27 21:03:08 +0600

    new css and minor updates

--- meetup_site/meetups/forms.py
+++ meetup_site/meetups/forms.py
@@ -1,17 +1,23 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from image_uploader_widget.widgets import ImageUploaderWidget
 
 from .models import Company
 
 
 class AddPostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = 'Категория не выбрана'
     class Meta:
         model = Company
         fields = ['title', 'slug', 'content', 'photo', 'cat']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-input'}),
-            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10})
+            'title': forms.Textarea(attrs={'class': 'form-input'}),
+            'slug': forms.Textarea(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10, 'class': 'form-input'}),
         }
+        fields = '__all__'
 
     def clean_title(self):
         title = self.cleaned_data['title']
