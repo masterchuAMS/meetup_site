2789ce072ab274d9cc617cafbe7eb8a60ba90eef
Coriolanus <savvarabota@gmail.com>
2023-04-06 19:44:00 +0300

    добавил миксин

--- meetup_site/meetups/utilits.py
+++ meetup_site/meetups/utilits.py
@@ -3,7 +3,7 @@ from meetups.models import Company
 class DataMixin:
     paginate_by = 1
     def get_user_context(self, **kwargs):
-        context = kwargs
+        context =    kwargs
         cats = Company.objects.all()
         context['cats'] = cats
         if 'cat_selected' not in context:
